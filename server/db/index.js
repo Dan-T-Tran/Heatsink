021374c7c268c0d3c54f5ef9d97a19fda6ff28d8
Nooby <dan.t.tran505@gmail.com>
2022-10-03 14:10:46 -0700

    connect to mongoDB Atlas

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/heatsink', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('mongodb://localhost:27017/heatsink', {useNewUrlParser: true, useUnifiedTopology: true});
 
+// mongoose.connect('mongodb://localhost:27017/heatsink', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect(`mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@heatsink-cluster.efwwkwt.mongodb.net/?retryWrites=true&w=majority
+`, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 const heatSchema = new mongoose.Schema({
   name: String,

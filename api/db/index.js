2f397a995f490f63da83008bf5925c4fa8643742
Nooby <dan.t.tran505@gmail.com>
2022-10-05 22:33:52 -0700

    reattempt integration

--- api/db/index.js
+++ api/db/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost:27017/heatsink', {useNewUrlParser: true, useUnifiedTopology: true});
 
-mongoose.connect(process.env.REACT_APP_MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 const db = mongoose.connection;

4a2ecfcdf497a18fff193ac5488a536895d9d3e4
Nooby <dan.t.tran505@gmail.com>
2022-10-05 19:06:10 -0700

    reattempt integration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.DBPORT || 5000;
+const port = process.env.DBPORT || 8080;
 const path = require('path');
 const { getScores, postScore } = require('./controllers');
 

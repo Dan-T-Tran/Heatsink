178f6b48f7565b62a8cc32db2cbb88d75b77e3c1
Nooby <dan.t.tran505@gmail.com>
2022-10-05 20:27:02 -0700

    reattempt integration

--- api/index.js
+++ api/index.js
@@ -7,8 +7,8 @@ const { getScores, postScore } = require('./controllers');
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '..//build')));
-app.use(express.static(path.join(__dirname, '..//public')));
+// app.use(express.static(path.join(__dirname, '..//build')));
+// app.use(express.static(path.join(__dirname, '..//public')));
 
 // app.use((req, res, next) => {
 //   res.sendFile(path.join(__dirname, "..", "heatsink", "build", "index.html"));
